26a5dae9841cdeec56e2d9fdb95c03ff0e146973
Unversedsix0 <gaba.olipires@gmail.com>
2022-12-29 16:06:15 -0300

    feat: add row.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import categories from './api';
 import './App.css';
+import Row from './components/Row';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {categories.map((category) =>{
+        return <Row key={category.name} 
+        title={category.title} 
+        path={category.path}
+        />;
+      })}
+      
+   
     </div>
   );
 }
